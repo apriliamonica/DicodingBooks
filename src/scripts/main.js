3656895ae426e32e3813b9282a9a200304e63630
April <liapandeiroth34190@gmail.com>
2024-10-14 23:21:14 +0800

    fetch insertBook

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -40,31 +40,51 @@ function main() {
   // xhr.send();
   // };
   const insertBook = (book) => {
-    // tuliskan kode di sini!
-    // Membuat instance dari XMLHttpRequest
-    const xhr = new XMLHttpRequest();
-
-    // Menetapkan callback jika response sukses dan error
-    xhr.onload = function () {
-      const responseJson = JSON.parse(this.responseText);
-      showResponseMessage(responseJson.message);
-      getBook();
-    };
-
-    xhr.onerror = function () {
-      showResponseMessage();
-    };
-
-    // Membuat POST request dan menetapkan target URL
-    xhr.open("POST", `${baseUrl}/add`);
-
-    // Menetapkan properti Content-Type dan X-Auth-Token pada Header request
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.setRequestHeader("X-Auth-Token", "12345");
-
-    // Mengirimkan request dan menyisipkan JSON.stringify(book) pada body
-    xhr.send(JSON.stringify(book));
+    fetch("${baseUrl}/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Auth-Token": "12345",
+      },
+      body: JSON.stringify(book),
+    })
+      .then((response) => {
+        response.json();
+        return response.json();
+      })
+      .then((responseJson) => {
+        showResponseMessage(responseJson.message);
+        getBook();
+      })
+      .catch((error) => {
+        showResponseMessage(error);
+      });
   };
+  // tuliskan kode di sini!
+  // Membuat instance dari XMLHttpRequest
+  // const xhr = new XMLHttpRequest();
+
+  // // Menetapkan callback jika response sukses dan error
+  // xhr.onload = function () {
+  //   const responseJson = JSON.parse(this.responseText);
+  //   showResponseMessage(responseJson.message);
+  //   getBook();
+  // };
+
+  // xhr.onerror = function () {
+  //   showResponseMessage();
+  // };
+
+  // // Membuat POST request dan menetapkan target URL
+  // xhr.open("POST", `${baseUrl}/add`);
+
+  // // Menetapkan properti Content-Type dan X-Auth-Token pada Header request
+  // xhr.setRequestHeader("Content-Type", "application/json");
+  // xhr.setRequestHeader("X-Auth-Token", "12345");
+
+  // // Mengirimkan request dan menyisipkan JSON.stringify(book) pada body
+  // xhr.send(JSON.stringify(book));
+  // };
 
   const updateBook = (book) => {
     // tuliskan kode di sini!
